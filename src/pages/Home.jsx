e61d49996d868282eed09cc8fe183f97b9843e64
Yash Kumar <yashkumar62011@gmail.com>
2026-06-15 18:49:12 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -135,7 +135,7 @@ export default function Home() {
                             <span>{location ? "Nearby You" : "Enable Location"}</span>
                         </button>
                     </div>
-                    <img src="/logo.png" alt="NoticeBoard AI" className="w-17 h-17 rounded-xl  object-cover" />
+                    <img src="/logo.png" alt="NearNotice" className="w-17 h-17 rounded-xl  object-cover" />
                 </div>
 
                 {/* Search Bar */}
